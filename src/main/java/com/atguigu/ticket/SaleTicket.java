48f5172c00e5f1a4495634b1c0613497e20dd09d
zhukehui <574059694@qq.com>
2019-10-11 19:58:47 +0800

    多线程售票实例

--- src/main/java/com/atguigu/ticket/SaleTicket.java
+++ src/main/java/com/atguigu/ticket/SaleTicket.java
@@ -4,10 +4,21 @@ package com.atguigu.ticket;
 public class SaleTicket {
 	public static void main(String[] args) {
 		Ticket ticket = new Ticket();
+		
 		new Thread(() -> {for (int i = 0; i < 40; i++) ticket.sale(); },"售票员一").start();
 		new Thread(() -> {for (int i = 0; i < 40; i++) ticket.sale(); },"售票员二").start();
 		new Thread(() -> {for (int i = 0; i < 40; i++) ticket.sale(); },"售票员三").start();
 
+		/**
+		 * 2    Lambda Express（前提是函数式接口，只有一个方法）
+		 *
+		 * 2.1 拷贝小括号，写死右箭头，落地大括号
+		 * 2.2 注解@FunctionalInterface
+		 * 2.3 default方法 可以有多个
+		 * 2.4 static方法  可以有多个
+		 *
+		 */		
+		
 		/*
 		 * new Thread(new Runnable() {
 		 * 
@@ -27,6 +38,8 @@ public class SaleTicket {
 
 	}
 }
+
+//三名售票员  卖票   30张
 
 class Ticket {// 资源类
 	private int ticket = 30;
