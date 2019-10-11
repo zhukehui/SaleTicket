893930fea4d77bd72bcf51b2a2e678c203704f8f
zhukehui <574059694@qq.com>
2019-10-11 20:05:20 +0800

    多线程售票实例

--- src/main/java/com/atguigu/ticket/SaleTicket.java
+++ src/main/java/com/atguigu/ticket/SaleTicket.java
@@ -9,15 +9,7 @@ public class SaleTicket {
 		new Thread(() -> {for (int i = 0; i < 40; i++) ticket.sale(); },"售票员二").start();
 		new Thread(() -> {for (int i = 0; i < 40; i++) ticket.sale(); },"售票员三").start();
 
-		/**
-		 * 2    Lambda Express（前提是函数式接口，只有一个方法）
-		 *
-		 * 2.1 拷贝小括号，写死右箭头，落地大括号
-		 * 2.2 注解@FunctionalInterface
-		 * 2.3 default方法 可以有多个
-		 * 2.4 static方法  可以有多个
-		 *
-		 */		
+	
 		
 		/*
 		 * new Thread(new Runnable() {
