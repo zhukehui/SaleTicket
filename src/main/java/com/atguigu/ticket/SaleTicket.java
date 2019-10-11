219487ef35b496e1c8bbc329b37a173afba24523
zhukehui <574059694@qq.com>
2019-10-11 18:02:17 +0800

    售票员多线程卖票

---
+++ src/main/java/com/atguigu/ticket/SaleTicket.java
@@ -0,0 +1,39 @@
+package com.atguigu.ticket;
+
+//在高内聚低耦合的前提下，线程      操作		资源类
+public class SaleTicket {
+	public static void main(String[] args) {
+		Ticket ticket = new Ticket();
+		new Thread(() -> {for (int i = 0; i < 40; i++) ticket.sale(); },"售票员一").start();
+		new Thread(() -> {for (int i = 0; i < 40; i++) ticket.sale(); },"售票员二").start();
+		new Thread(() -> {for (int i = 0; i < 40; i++) ticket.sale(); },"售票员三").start();
+
+		/*
+		 * new Thread(new Runnable() {
+		 * 
+		 * @Override public void run() { for (int i = 0; i < 40; i++) { ticket.sale(); }
+		 * } }, "售票员一").start();
+		 *  
+		 * new Thread(new Runnable() {
+		 * 
+		 * @Override public void run() { for (int i = 0; i < 40; i++) { ticket.sale(); }
+		 * } }, "售票员二").start();
+		 * 
+		 * new Thread(new Runnable() {
+		 * 
+		 * @Override public void run() { for (int i = 0; i < 40; i++) { ticket.sale(); }
+		 * } }, "售票员三").start();
+		 */
+
+	}
+}
+
+class Ticket {// 资源类
+	private int ticket = 30;
+
+	public synchronized void sale() {
+		if (ticket > 0) {
+			System.out.println(Thread.currentThread().getName() + "正在卖第" + (ticket--) + "张票，还剩" + ticket + "张票");
+		}
+	}
+}
---
+++ src/main/java/com/atguigu/ticket/Test.java
@@ -0,0 +1,10 @@
+package com.atguigu.ticket;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a = 1;
+		for (int i = 0; i < 10; i++) System.out.println(i);
+		if (a>0) System.out.println(a);
+	}
+}
